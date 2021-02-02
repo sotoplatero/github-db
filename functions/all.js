313b7782e3ebd93196c3ccc1f4ba6f441d975eb8
Damian Soto <soto.platero@gmail.com>
2021-02-01 23:33:55 -0500

    add gitignore

--- functions/all.js
+++ functions/all.js
@@ -5,13 +5,6 @@ const { Octokit } = require("@octokit/rest");
 /* export our lambda function as named "handler" export */
 exports.handler = async function(event, context) {
 
-  let todo = JSON.parse( event.body )
-
-  todo = {
-    id: uuidv4(),
-    ...todo,
-  };
-
   const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
   });
@@ -23,22 +16,13 @@ exports.handler = async function(event, context) {
   });
 
   let todos = Buffer.from(data.content, 'base64').toString('utf8') 
-  todos = [ ...JSON.parse(todos), todo ];
-
-  var fileContent = Buffer.from( JSON.stringify( todos ), 'utf8' ).toString('base64') 
-
-  await octokit.repos.createOrUpdateFileContents({
-      owner: 'sotoplatero',
-      repo: 'db',
-      path: 'todos.json',
-      message: '+',
-      sha: data.sha,
-      content: fileContent,
-    })
 
   return {
+    headers: { 
+        'Content-Type':'application/json' , 
+    },    
     statusCode: 200,
-    body: JSON.stringify(todos)
+    body: todos
   }     
 
 }
