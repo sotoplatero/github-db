f59abfaa1cb145733cc48179f2e58ea59664d408
Damian Soto <soto.platero@gmail.com>
2021-02-02 16:10:51 -0500

    update

--- functions/delete.js
+++ functions/delete.js
@@ -4,13 +4,7 @@ const { Octokit } = require("@octokit/rest");
 /* export our lambda function as named "handler" export */
 exports.handler = async function(event, context) {
 
-  let todo = JSON.parse( event.body )
-
-  todo = {
-    id: uuidv4(),
-    done: false,
-    ...todo,
-  };
+  let { id } = JSON.parse( event.body )
 
   const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
@@ -22,8 +16,11 @@ exports.handler = async function(event, context) {
     path: 'todos.json',
   });
 
+  // decodificar el json
   let todos = Buffer.from(data.content, 'base64').toString('utf8') 
-  todos = [ ...JSON.parse(todos), todo ];
+
+  // eliminar el todo 
+  todos = JSON.parse(todos).filter( el => el.id != id );
 
   var fileContent = Buffer.from( JSON.stringify( todos ), 'utf8' ).toString('base64') 
 
--- functions/update.js
+++ functions/update.js
@@ -5,13 +5,6 @@ const { Octokit } = require("@octokit/rest");
 exports.handler = async function(event, context) {
 
   let todo = JSON.parse( event.body )
-
-  todo = {
-    id: uuidv4(),
-    done: false,
-    ...todo,
-  };
-
   const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
   });
@@ -22,8 +15,12 @@ exports.handler = async function(event, context) {
     path: 'todos.json',
   });
 
-  let todos = Buffer.from(data.content, 'base64').toString('utf8') 
-  todos = [ ...JSON.parse(todos), todo ];
+  let todosString = Buffer.from( data.content, 'base64' ).toString( 'utf8' ) 
+  todos = JSON.parse( todosString );
+
+  todoIndex = todos.findIndex( el => el.id === todo.id );
+  console.log(todo)
+  todos.splice( todoIndex, 1, todo)
 
   var fileContent = Buffer.from( JSON.stringify( todos ), 'utf8' ).toString('base64') 
 
